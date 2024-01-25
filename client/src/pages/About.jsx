59639e1d5e458184b5c281a127ced20a1600e0cd
Ryan England <me@rengland.org>
2024-01-24 22:07:54 -0800

    Added some about page logic

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 
 const About = () => {
-    return(
+    return (
         <Container className="about">
-            <div>Coming soon!</div>
+            <Container className="about-title my-5">
+                <h2 className="about-header text-end">Elisha Aflalo is a multimedia artist currently based in San Francisco.</h2>
+                <h3 className="about-subheader text-start">She is a graduate of Bennington College where she studied poetry, printmaking, and performance.</h3>
+            </Container>
         </Container>
     );
 };
